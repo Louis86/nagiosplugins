a4faadb072e06e321010e361338f3233977da74f
Louis86 <louishuguesa@gmail.com>
2019-03-04 14:56:34 +0100

    Update 1cpucheck.py

--- 1cpucheck.py
+++ 1cpucheck.py
@@ -33,17 +33,37 @@ class Load(nagiosplugin.Resource):
         for i, period in enumerate([1, 5, 15]):
             yield nagiosplugin.Metric('load%d' % period, load[i], min=0, context='load')
 
+class LoadSummary(nagiosplugin.Summary):
+    """Status line conveying load information.
+
+    We specialize the `ok` method to present all three figures in one
+    handy tagline. In case of problems, the single-load texts from the
+    contexts work well.
+    """
+
+    def __init__(self, percpu):
+        self.percpu = percpu
+
+    def ok(self, results):
+        qualifier = 'per cpu ' if self.percpu else ''
+        return 'loadavg %sis %s' % (qualifier, ', '.join(
+            str(results[r].metric) for r in ['load1', 'load5', 'load15']))
+
 def main():
     argp = argparse.ArgumentParser(description=__doc__)
-    argp.add_argument('-w', '--warning', metavar='RANGE', default='', help='return warning if load is outside RANGE')
-    argp.add_argument('-c', '--critical', metavar='RANGE', default='', help='return critical if load is outside RANGE')
+    argp.add_argument('-w', '--warning', metavar='RANGE', default='',
+                      help='return warning if load is outside RANGE')
+    argp.add_argument('-c', '--critical', metavar='RANGE', default='',
+                      help='return critical if load is outside RANGE')
     argp.add_argument('-r', '--percpu', action='store_true', default=False)
+    argp.add_argument('-v', '--verbose', action='count', default=0,
+                      help='increase output verbosity (use up to 3 times)')
     args = argp.parse_args()
     check = nagiosplugin.Check(
         Load(args.percpu),
-        nagiosplugin.ScalarContext('load', args.warning, args.critical))
-    check.main()
-
+        nagiosplugin.ScalarContext('load', args.warning, args.critical),
+        LoadSummary(args.percpu))
+    check.main(verbose=args.verbose)
 
 if __name__ == '__main__':
     main()
