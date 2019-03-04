492e085c4c4119378db6e0be7852d96702b19617
Louis86 <louishuguesa@gmail.com>
2019-03-04 13:43:02 +0100

    Update 1cpucheck.py

--- 1cpucheck.py
+++ 1cpucheck.py
@@ -36,12 +36,12 @@ def main():
     argp = argparse.ArgumentParser(description=__doc__)
     argp.add_argument('-w', '--warning', metavar='RANGE', default='', help='return warning if load is outside RANGE')
     argp.add_argument('-c', '--critical', metavar='RANGE', default='', help='return critical if load is outside RANGE')
-        argp.add_argument('-r', '--percpu', action='store_true', default=False)
-        args = argp.parse_args()
-        check = nagiosplugin.Check(
-            Load(args.percpu),
-            nagiosplugin.ScalarContext('load', args.warning, args.critical))
-        check.main()
+    argp.add_argument('-r', '--percpu', action='store_true', default=False)
+    args = argp.parse_args()
+    check = nagiosplugin.Check(
+        Load(args.percpu),
+        nagiosplugin.ScalarContext('load', args.warning, args.critical))
+    check.main()
 
 
 if __name__ == '__main__':
