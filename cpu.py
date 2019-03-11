6ca85a9f82a1d38e90fe92bf6f2514f4173a2cbc
Louis86 <louishuguesa@gmail.com>
2019-03-11 10:02:57 +0100

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -117,13 +117,13 @@ def connect():
 def main():
     arg()
     print(connect())
-    if connect()[3] =! 0:
+    if connect()[3] != 0:
         sys.exit(CRITICAL)
-    elif connect()[2] =! 0:
+    elif connect()[2] != 0:
         sys.exit(WARNING)
-    elif connect()[4] =! 0:
+    elif connect()[4] != 0:
         sys.exit(UNKNOWN)
-    else connect()[1] =! 0:
+    else connect()[1] != 0:
         sys.exit(OK)
 
 
