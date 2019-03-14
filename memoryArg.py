c4e8287bff3bf026bb77eb1abc6401fb657c05e3
Louis86 <louishuguesa@gmail.com>
2019-03-14 13:55:11 +0100

    Update memoryArg.py

--- memoryArg.py
+++ memoryArg.py
@@ -1,8 +1,10 @@
 import argparse
 parser = argparse.ArgumentParser(description="Plugin shows the memory state in terms of memory percentage")
-parser.add_argument("-memoryOK", "-W","-C", help="range of memory OK", type=int)
+parser.add_argument("memoryOK" ,help="range of memory OK", type=int)
+parser.add_argument("warning" ,help="range of memory warning", type=int)
+parser.add_argument("critical" ,help="range of memory warning", type=int)
 args = parser.parse_args()
 
 print(args.memoryOK)
-print(args.W)
-print(args.C)
+print(args.warning)
+print(args.critical)
