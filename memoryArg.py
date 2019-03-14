e1fab977c6bee4c0b37ae9c143e21a4ac8d663f1
Louis86 <louishuguesa@gmail.com>
2019-03-14 15:14:52 +0100

    Update memoryArg.py

--- memoryArg.py
+++ memoryArg.py
@@ -1,10 +1,10 @@
 import argparse
 parser = argparse.ArgumentParser(description="Plugin shows the memory state in terms of memory percentage")
 parser.add_argument('-s', '--size', nargs=1, required=False, help='disk size in percentage', dest='disk_size', type=str, default=['85%'])
-parser.add_argument("memoryOKmax" ,help="range of memory OK", type=int)
-parser.add_argument("warningMin" ,help="range of memory warning", type=int)
-parser.add_argument("warningMax" ,help="range of memory warning Maximum", type=int)
-parser.add_argument("criticalMin" ,help="range of memory critical Minimum", type=int)
+parser.add_argument("memoryOKmax" ,help="Maximum of memory OK", type=int)
+parser.add_argument("warningMin" ,help="rMinimum of memory warning", type=int)
+parser.add_argument("warningMax" ,help="Maximum of memory : state warning ", type=int)
+parser.add_argument("criticalMin" ,help="Minimum of memory :  stage critical ", type=int)
 args = parser.parse_args()
 
 print(args.memoryOKmax)
