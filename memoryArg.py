20dbe4540858b0238f1b27074e27bc2767387e93
Louis86 <louishuguesa@gmail.com>
2019-03-14 14:25:07 +0100

    Update memoryArg.py

--- memoryArg.py
+++ memoryArg.py
@@ -1,5 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser(description="Plugin shows the memory state in terms of memory percentage")
+parser.add_argument('-s', '--size', nargs=1, required=False, help='disk size in percentage', dest='disk_size', type=str, default=['85%'])
 parser.add_argument("memoryOK" ,help="range of memory OK", type=int)
 parser.add_argument("warning" ,help="range of memory warning", type=int)
 parser.add_argument("critical" ,help="range of memory warning", type=int)
