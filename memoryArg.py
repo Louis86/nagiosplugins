7ca2faf3f9247a0416c88b9766a53e414caad7d4
Louis86 <louishuguesa@gmail.com>
2019-03-14 13:13:53 +0100

    Update memoryArg.py

--- memoryArg.py
+++ memoryArg.py
@@ -2,11 +2,5 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Plugin shows the memory state in terms of memory percentage')
 parser.add_argument('memoryOK' ,type= int, required=True help='range of memory OK')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
-
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+print(args.memoryOK)
