9250040c38aed5fdfb7fd6e9f8d87be2cb03a708
Louis86 <louishuguesa@gmail.com>
2019-03-14 16:09:48 +0100

    Update memoryArg.py

--- memoryArg.py
+++ memoryArg.py
@@ -1,6 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser(description="Plugin shows the memory state in terms of memory percentage")
-parser.add_argument('-Ok',dest="memoryOKmax" ,help="percentage Maximum of memory OK", type=int ,required=True choices=range(100))
+parser.add_argument('-Ok',dest="memoryOKmax" ,help="percentage Maximum of memory OK", type=int ,required=True,choices=range(100))
 parser.add_argument('-wMin',dest="warningMin" ,help="percentage Minimum of memory warning", type=int ,required=True)
 parser.add_argument('-wMax',dest="warningMax" ,help="percentage Maximum of memory : state warning", type=int ,required=True)
 parser.add_argument('-cMin',dest="criticalMin" ,help="percentage Minimum of memory :  stage critical", type=int ,required=True)
