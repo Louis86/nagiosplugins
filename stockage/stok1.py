d978405178353d72bafff74bb5c5d1b05012c87d
Louis86 <louishuguesa@gmail.com>
2019-03-22 13:56:55 +0100

    Update stok1.py

--- stockage/stok1.py
+++ stockage/stok1.py
@@ -24,4 +24,7 @@ vms = datacenter.hostFolder.childEntity
 #Iterating each vm object and printing its name
 for i in vms:
     print(i.name)
+    hosts = i.host
+    for host in hosts:
+        print(host.name)
 Disconnect(c)
